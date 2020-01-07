b4fea77e5cef07fa63ddffedd21cedaeccff0b12
Yuliya <yuliyababarykina@gmail.com>
2020-01-07 18:09:05 -0500

    Updates

---
+++ precache-manifest.4d883f43aadca7128930bb5b131e36ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdb79d037ab8ed4cd2f61175057df82",
+    "url": "/React_image_finder/index.html"
+  },
+  {
+    "revision": "ee8ba1b1aa4f7a1c69ba",
+    "url": "/React_image_finder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "684d86a862eaac80f4c4",
+    "url": "/React_image_finder/static/js/2.e60949e9.chunk.js"
+  },
+  {
+    "revision": "f4dd3ea946eda1151bd0ef4fc95a28d5",
+    "url": "/React_image_finder/static/js/2.e60949e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee8ba1b1aa4f7a1c69ba",
+    "url": "/React_image_finder/static/js/main.d64311dd.chunk.js"
+  },
+  {
+    "revision": "ac7eeae0f59d51ff0416",
+    "url": "/React_image_finder/static/js/runtime-main.634ab00c.js"
+  }
+]);
